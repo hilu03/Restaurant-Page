1a012b935e8c0cb446e4878a55fbc59ae100159f
Luong Tran <tranthihienluong2003@gmail.com>
2024-08-25 22:57:52 +0700

    Style contact page

--- src/contact.js
+++ src/contact.js
@@ -5,28 +5,28 @@ export function createContactDiv() {
   `
     <h1>Contact Us</h1>
     <div class="contact">
-      <div class="contact-name">Mr. Gold</div>
-      <div class="info">
-        <div class="position">Manager</div>
-        <div class="phone">333-333-3333</div>
-        <div class="mail">golden@hilu.com</div>
-      </div>
+      <h2 class="contact-name">Mr. Gold</h2>
+      <ul class="info">
+        <li class="position">Position: Manager</li>
+        <li class="phone">Phone: 333-333-3333</li>
+        <li class="mail">Email: golden@hilu.com</li>
+      </ul>
     </div>
     <div class="contact">
-      <div class="contact-name">Ms. Milo</div>
-      <div class="info">
-        <div class="position">Chef</div>
-        <div class="phone">310-310-0310</div>
-        <div class="mail">milo@hilu.com</div>
-      </div>
+      <h2 class="contact-name">Ms. Milo</h2>
+      <ul class="info">
+        <li class="position">Position: Chef</li>
+        <li class="phone">Phone: 310-310-0310</li>
+        <li class="mail">Email: milo@hilu.com</li>
+      </ul>
     </div>
     <div class="contact">
-      <div class="contact-name">Mr. White</div>
-      <div class="info">
-        <div class="position">Waiter</div>
-        <div class="phone">333-333-3332</div>
-        <div class="mail">white@hilu.com</div>
-      </div>
+      <h2 class="contact-name">Mr. White</h2>
+      <ul class="info">
+        <li class="position">Position: Waiter</li>
+        <li class="phone">Phone: 333-333-3332</li>
+        <li class="mail">Email: white@hilu.com</li>
+      </ul>
     </div>
   `;
   div.innerHTML = html;
